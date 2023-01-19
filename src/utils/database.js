25ca3cf8eee81440060a1b243de51fb661bd48b3
Diego Garay <diegogaraycullas@gmail.com>
2023-01-19 08:45:07 -0500

    se agrego archivos env y envexample

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,13 +1,14 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config()
 
 //crear una instancia con parametros de confiiguracion de la database
 //necesitamos qun objeto de configuracion que no es mas que las credenciales de mi database
 const db = new Sequelize({
-  database: "todosapp2",   //*********** */
-  username: "postgres",      //nuestro usuario
-  host: "localhost",      //127.0.0.1
-  port: "5432",
-  password: "021087Mikeyla",     //tu propia contraseña
+  database: process.env.DB_NAME,   //*********** */
+  username: process.env.DB_USER,      //nuestro usuario
+  host: process.env.DB_HOST,      //127.0.0.1
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,     //tu propia contraseña
   dialect: "postgres",     //la base de datos que estamos usando
   logging: false,      //silencia toda la informacion que da el sequelize en la consola
 })
